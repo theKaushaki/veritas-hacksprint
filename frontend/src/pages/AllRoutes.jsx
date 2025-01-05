c7d38e499e891d1b46f7ef8371a9e969dfb9ecc1
GirishIITM <23f2005217@ds.study.iitm.ac.in>
2025-01-05 09:42:06 +0530

    Add routing for departments, procedures, students, superAdmin, and notifications; implement StudentDashboard page

--- frontend/src/pages/AllRoutes.jsx
+++ frontend/src/pages/AllRoutes.jsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './Home'
 import FacultyHome from "./FacultyHome";
+import StudentDashboard from './StudentDashboard';
 
 export default function AllRoutes() {
     return (
         <Routes>
             <Route path='/' element={<Home />} />
             <Route path='*' element={<h1>Not Found</h1>} />
-            <Route path='/faculty' element={<FacultyHome/>} />
+            <Route path='/faculty' element={<FacultyHome />} />
+            <Route path='/student' element={<StudentDashboard />} />
         </Routes>
     )
 }
--- frontend/src/pages/StudentDashboard.jsx
+++ frontend/src/pages/StudentDashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './StudentDashboard.css';
+// import './StudentDashboard.css';
 
 // FormsPage Component
 const FormsPage = () => {
